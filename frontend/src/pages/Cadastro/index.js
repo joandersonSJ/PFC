4144cce8893a3459095123bdcdfc71527450b620
joandersonSJ <joandersonsantosdev@gmail.com>
2020-07-30 16:39:26 -0300

    :rocket: requisição a api de municipios criada

--- frontend/src/pages/Cadastro/index.js
+++ frontend/src/pages/Cadastro/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import background_unidades from "../../assets/background_unidades.svg";
 
 import {
@@ -10,11 +10,28 @@ import {
 } from "./styles";
 
 export default function Cadastro() {
+  const [municipios, setMunicipios] = useState([]);
+  useEffect(() => {
+    async function searchItem() {
+      const municipios = await fetch(
+        "https://servicodados.ibge.gov.br/api/v1/localidades/estados/BA/municipios"
+      );
+      const municipios_in_json = await municipios.json();
+      setMunicipios(municipios_in_json);
+    }
+    searchItem();
+  }, []);
+
   return (
     <Container>
       <Image src={background_unidades} alt="background" />
       <FormularioContainer>
         <Formulario>
+          <select>
+            {municipios.map((municipio) => (
+              <option value={municipio.nome}>{municipio.nome}</option>
+            ))}
+          </select>
           <Input type="text" />
           <Input type="text" />
           <Input type="text" />
